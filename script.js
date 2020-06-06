09a82370026217c708b994a1da9b29c939fedc87
tonia-jam <66325349+tonia-jam@users.noreply.github.com>
2020-06-06 14:23:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let additionalBlock = {
   };
 
 $('body').append(
-    `<h1>${additionalBlock.title}</h1>`,   
+    `<h1>${additionalBlock.title}</h1>`,
     `<p>${additionalBlock.text}</p>`,
 );
 
