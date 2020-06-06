bc87f7c43e514cef2dd44d19ce7720582846f57d
tonia-jam <66325349+tonia-jam@users.noreply.github.com>
2020-06-06 13:43:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ names.forEach((name) => {
     $("ul").append(
         `<li>${name}</li>`
 )});
+
+let additionalBlock = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
+  };
+
+$('body').append(
+    `<h1>${additionalBlock.title}</h1>`    
+);
+$('body').append(
+    `<p>${additionalBlock.text}</p>`
+);
